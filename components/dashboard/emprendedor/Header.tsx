f11a64e0648b497089f34b78fb3f53693dcd1c52
Alexa Genar <gomezgenesisalexa03@gmail.com>
2025-10-04 14:27:29 -0600

    feat: notifications

--- components/dashboard/emprendedor/Header.tsx
+++ components/dashboard/emprendedor/Header.tsx
@@ -3,17 +3,26 @@
 import * as React from "react";
 import { Button } from "@/components/ui/button";
 import { Menu } from "lucide-react";
+import { NotificationsModal } from "./notificationsModal";
 
 export default function Header({ onOpenSidebar }: { onOpenSidebar: () => void }) {
   return (
     <header className="sticky top-0 z-40 border-b bg-background/80 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="flex h-16 items-center gap-3 px-4 md:pl-72">
+      <div className="flex h-16 items-center gap-3 px-4 md:pl-4">
         <div className="md:hidden">
           <Button variant="ghost" size="icon" aria-label="Abrir menú" onClick={onOpenSidebar}>
             <Menu className="h-5 w-5" />
           </Button>
         </div>
-        <h1 className="text-lg font-semibold tracking-tight">Chorotega E-Market</h1>
+        <img
+            src="/Chorotega.svg" 
+            alt="Logo Chorotega"
+            width={50}
+            height={50}
+            className="rounded-sm object-contain"
+          />
+        <h1 className="text-lg font-semibold tracking-tight">Chorotega E-Market</h1>\
+        <NotificationsModal/>
         <div className="ml-auto" />
       </div>
     </header>
